e68d091a4c959adb079bb7e8fbd59f38d31f6f23
Rodney R <majid.muhassan@gmail.com>
2018-01-23 17:18:23 +0000

    added axios dependency and a QueryApi.js file.  This allows an ajax call to 10 different crypco currencies and compare them with 20 countries.  The data gives the current value with an appxoximate 5 minute latency.

--- crypto-app/src/QueryApi.js
+++ crypto-app/src/QueryApi.js
@@ -2,5 +2,6 @@ const axios = require ('axios');
 const queryUrl1 = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=LTD,42,PBT,BTC,BCH,ETH,DASH,XMR,LTC,CND&tsyms=USD,EUR,GBP,JPY,AED,INR,CAD,AUD,SGD,BHD,BRL,CHF,CNY,HKD,IDR,IRR,KWD,KRW,MYR,MXN,SAR";
 
 axios.get(queryUrl1).then(function(cryptoCoins){
-   console.log(cryptoCoins.data); //console.log("BTC :", cryptoCoins.data.BTC);
+//   console.log(cryptoCoins.data); 
+console.log("BTC :", cryptoCoins.data.BTC.USD);
 });
