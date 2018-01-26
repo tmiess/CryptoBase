c7a5d708d3f62d3b207f677d916ef2afced4634e
Rodney R <majid.muhassan@gmail.com>
2018-01-26 19:17:40 +0000

    The socketio chat now works.  You can make a psuedo username, the logic will not allow the  same name to be active at the same time, it's been tested to work through two separate browsers. There's also a restructuring of the file tree in this branch to bring it closer to MVC and how prior react app assingmets are setup.

---
+++ crypto-app/server.js
@@ -0,0 +1,50 @@
+const express = require("express"),
+        app = express(),
+        server = require('http').Server(app),
+        io = require('socket.io')(server),
+        PORT = process.env.PORT || 8081;
+    let usernames=[]; 
+        
+app.use(express.static("client/build"));        
+server.listen(PORT,() => {
+      console.log(`🌎 ==> Server now on port ${PORT}!`);
+});
+
+
+//first route to index.html
+//"*" will be changed to the appropriate url path name later
+app.get("*",(req,res)=> {
+    res.sendFile(__dirname + "/client/src/components/ChatIo/index.html"); //check here if chat doesn't load
+});
+
+io.sockets.on("connection", (socket)=>{
+    console.log("socket connected");
+    
+    socket.on("new user", (data,callback)=>{ //logic to make sure the same person can't be logged in
+        if(usernames.indexOf(data) != -1){
+            callback(false);
+        }else{
+            callback(true); // if it is true
+            socket.username = data; // then grab the user's data then
+            usernames.push(socket.username);//and put it in the usernames array
+            updateUsernames();
+        }
+    });
+    
+    const updateUsernames = () => {  //obviously this handles updating the username. :D
+        // console.log(username)
+        io.emit("usernames", usernames);
+    };
+    socket.on("send message", (data)=>{ //this sends the message
+        io.emit("new massage", {msg:data, user:socket.username}); //this catches the message and "emits" the data to client
+    });
+    
+    socket.on("disconnect", (data)=>{
+        if(!socket.username){
+            return;
+        }
+        usernames.splice(usernames.indexOf(socket.username), 1);
+        updateUsernames();
+    });
+});
+
