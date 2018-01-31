d5f1f272c4a111c9461aff01a90cebabb13347ab
Rodney R <majid.muhassan@gmail.com>
2018-01-31 12:19:51 +0000

    added socket.io dependency to the package.json in the server directory.  Added the code to handle socket.io on the server.js file

--- jeff/server/server.js
+++ jeff/server/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const path = require("path");
-const PORT = process.env.PORT;
-const app = express();
+const PORT = process.env.PORT || 8081;
+const app = express(),
+      server = require('http').Server(app),
+      io = require('socket.io')(server);
+  let usernames=[];     
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
@@ -16,4 +19,38 @@ app.get("*", function(req, res) {
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
+});
+
+//////socket io///////
+
+io.sockets.on("connection", (socket)=>{
+    console.log("socket connected");
+    
+    socket.on("new user", (data,callback)=>{ //logic to make sure the same person can't be logged in
+    console.log("data from server.js"+ data);
+        if(usernames.indexOf(data) != -1){
+            callback(false);
+        }else{
+            callback(true); // if it is true
+            socket.username = data; // then grab the user's data then
+            usernames.push(socket.username);//and put it in the usernames array
+            updateUsernames();
+        }
+    });
+    
+    const updateUsernames = () => {  //obviously this handles updating the username. :D
+        // console.log(username)
+        io.emit("usernames", usernames);
+    };
+    socket.on("send message", (data)=>{ //this sends the message
+        io.emit("new massage", {msg:data, user:socket.username}); //this catches the message and "emits" the data to client
+    });
+    
+    socket.on("disconnect", (data)=>{
+        if(!socket.username){
+            return;
+        }
+        usernames.splice(usernames.indexOf(socket.username), 1);
+        updateUsernames();
+    });
 });
