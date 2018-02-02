38546abfab9e98fe3a11ce42f25983a0b2d4e014
Rodney R <majid.muhassan@gmail.com>
2018-02-02 01:58:18 +0000

    update, still working on the socket.io.  Fix an error on the server.js and reconfigured the Chat.js form inputs and how the value of the inputs is handled.  Currently getting error 503.  Running into RAM issues in cloud9

--- jeff/server/client/src/components/ChatPage.js
+++ jeff/server/client/src/components/ChatPage.js
@@ -9,71 +9,72 @@ const socket = socketIOClient(`https://crypto-forum-majidmu.c9users.io:8081`); /
 class ChatPage extends Component {
 
   state = {
-    username: [],
-    value: "",
-    comments: [],
-
+    username: "",
+    message: "",
+    users: [],
+    messages: []
   };
 
   componentDidMount() {
-    socket.on('username', (data) => {
+    socket.on('new user', (data) => {
       this.setState({
-        username: [data, ...this.state.username]
+        users: [...data, this.state.username]
       });
     });
 
-    socket.on("new message", (data) => {
+    socket.on('new message', (data) => {
       this.setState({
-        comments: [data, ...this.state.comments]
-
-      })
+        messages: [...data, this.state.messages]
+      });
     });
   }
-  handleChangee = event => {
-    event.preventDefault();
-    this.state({
-      value: event.target.value
-    });
+  
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
   }
 
-  post = (event) => {
+  handleSubmit = (event) => {
     event.preventDefault();
-    socket.emit('new user', this.state.value);
+    socket.emit('new user', this.state.username);
   }
 
   render() {
-
     return (
       <div>
-      <h1>Chat</h1>
-      <div id= "container">
-          <div id= "namesWrapper">
-              <h2>Crypto Talk</h2>        
-              <p>Create UserName:</p>
-              <div id="error"></div>
-              <form id= "userNameForm">
-                 <input type="text" name="" size="35" id="username"/>
-                 <input type="submit" value="Submit" />
-              </form>
-          </div>
-          <div id="mainWrapper">
-              <h2>Crypto Talk</h2>
-              <div id="chatWrapper">
-                 <div id="chatWindow"></div>
-                  <form id="messageForm">
-                      <input type="text" name="" size="35" id="message" placeholder="Lets talk Cryto"/>
-                      <input type="submit" value="Submit"/>
-                  </form>
+        <h1>Chat</h1>
+          <div id= "container">
+              <div id= "namesWrapper">
+                <h2>Crypto Talk</h2>        
+                  <p>Create UserName:</p>
+                    <div id="error"></div>
+                    <form id= "userNameForm" onSubmit={this.handleSubmit}>
+                       <input type="text" name="username" size="35" id="username" value={this.state.username} onChange={this.handleChange}/>
+                      <input type="submit" value="Submit" />
+                    </form>
               </div>
-              <div id="userWrapper">
-                  <div id="users"></div>
+              <div id="mainWrapper">
+                  <h2>Crypto Talk</h2>
+                    <div id="chatWrapper">
+                     <div id="chatWindow">
+                       {this.state.messages.map(message => {
+                          return (<div>{message.username}: {message.message}</div>);
+                        })}
+                     </div>
+                      <form id="messageForm">
+                          <input type="text" name="message" size="35" id="message" value={this.state.message} onChange={this.handleChange} placeholder="Lets talk Cryto" />
+                          <input type="submit" value="Submit" />
+                      </form>
+                  </div>
+                  <div id="userWrapper">
+                      <div id="users"></div>
+                  </div>
+                  <div id="myName"></div>
               </div>
-              <div id="myName"></div>
           </div>
       </div>
-  </div>
     );
   }
-
 }
+
 export default ChatPage;
--- jeff/server/server.js
+++ jeff/server/server.js
@@ -1,14 +1,19 @@
 const express = require("express");
+const logger = require("morgan");
 const path = require("path");
+
 const PORT = process.env.PORT;
+
 const app = express(),
-      server = require('http').Server(app),
-      io = require('socket.io')(server);
-  let usernames=[];     
+    server = require('http').Server(app),
+    io = require('socket.io')(server);
+let usernames = [];
+
+app.use(logger("dev"));
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+    app.use(express.static("client/build"));
 }
 
 // Send every request to the React app
@@ -17,45 +22,51 @@ if (process.env.NODE_ENV === "production") {
 //   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 // });
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
-app.get("/chat",(req,res)=> {
+app.get("/chat", (req, res) => {
     res.sendFile(__dirname + "./client/src/components/ChatPage.js"); //check here if chat doesn't load
 });
 
-
-app.listen(PORT, function() {
-  console.log(`🌎 ==> Server now on port ${PORT}!`);
+server.listen(PORT, function() {
+    console.log(`🌎 ==> Server now on port ${PORT}!`);
 });
 
 
 
 //////socket io///////
 
-io.sockets.on("connection", (socket)=>{
+io.sockets.on("connection", (socket) => {
     console.log("socket connected");
-    
-    socket.on("new user", (data,callback)=>{ //logic to make sure the same person can't be logged in
-    console.log("data from server.js"+ data);
-        if(usernames.indexOf(data) != -1){
+
+    socket.on("new user", (data, callback) => { //logic to make sure the same person can't be logged in
+        console.log("data from server.js" + data);
+        if (usernames.indexOf(data) != -1) {
             callback(false);
-        }else{
+        }
+        else {
             callback(true); // if it is true
             socket.username = data; // then grab the user's data then
-            usernames.push(socket.username);//and put it in the usernames array
+            usernames.push(socket.username); //and put it in the usernames array
             updateUsernames();
         }
     });
-    
-    const updateUsernames = () => {  //obviously this handles updating the username. :D
+
+    const updateUsernames = () => { //obviously this handles updating the username. :D
         // console.log(username)
         io.emit("usernames", usernames);
     };
-    socket.on("send message", (data)=>{ //this sends the message
-        io.emit("new massage", {msg:data, user:socket.username}); //this catches the message and "emits" the data to client
-    });
     
-    socket.on("disconnect", (data)=>{
-        if(!socket.username){
+    socket.on("send message", (data) => { //this sends the message
+        io.emit("new massage", { msg: data, user: socket.username }); //this catches the message and "emits" the data to client
+    });
+
+    socket.on("disconnect", (data) => {
+        if (!socket.username) {
             return;
         }
         usernames.splice(usernames.indexOf(socket.username), 1);
