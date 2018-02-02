5144872fa156144e6d557f3a01e366c1b8b44bf5
Tyler Miess <tmiess19@gmail.com>
2018-02-01 22:25:02 -0500

    Update HomePage.js

--- jeff/server/client/src/components/HomePage.js
+++ jeff/server/client/src/components/HomePage.js
@@ -4,6 +4,9 @@ export default () => (
     <div id="intro">
         <h2>Welcome to CryptoBase, a crypto-community for the crypto-curious.</h2>
         <br></br>
-        <p>On this site you can find real rime rate conversion of the most popular cryptocurrencies, track monthly trends. Visit <strong>SEE</strong> to interact with monthly price charts of different currencies like Bitcoin, Ethereum, Litecoin, and 42. Never heard of concepts like blockchain or cryptocurrencies? Go to  <strong>LEARN</strong> for a comprehensive FAQ page. Are you a seasoned expert in the crypto-world? Head over to our <strong>TALK</strong> section and discuss current topics, ideas, or questions with other enthusiasts. Regardless of your experience, CryptoBase is your home for all things crypto.</p>
+        <p>On this site you can find real-time rate conversion of the most popular cryptocurrencies, track monthly trends, and communicate with crypto enthusiasts around the world. <br></br>
+        Visit <strong>SEE</strong> to interact with monthly price charts of different currencies like Bitcoin, Ethereum, Litecoin, and 42.
+        <br></br>Never heard of concepts like blockchain or cryptocurrencies? Go to  <strong>LEARN</strong> for a comprehensive FAQ page. 
+        <br></br>Are you a seasoned expert in the crypto-world? Head over to our <strong>TALK</strong> section and discuss current topics, ideas, or questions with other enthusiasts. <br></br>Regardless of your experience, CryptoBase is your home for all things crypto.</p>
     </div>
 );
